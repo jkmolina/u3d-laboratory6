457df6e365be3ae81b62fb3e519399f01a54b860
Joonho D. Kim <37599608+jkmolina@users.noreply.github.com>
2020-02-17 10:29:15 -0600

    Final commit Lab 6

---
+++ Laboratorio 2D/Assets/Scripts/Fox.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Fox : MonoBehaviour
+{
+    public GameObject fox;
+    public AudioSource fair;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void resetLevel()
+    {
+        SceneManager.LoadScene("SampleScene");
+        Movement.guapo = false;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (gameObject.CompareTag("Randy")&&(!Movement.guapo))
+        {
+            Debug.Log("mario dies");
+            Destroy(other.gameObject);
+        } 
+
+        else
+        {
+            Destroy(gameObject);
+            fair.Play();
+        }
+    }
+}
---
+++ Laboratorio 2D/Assets/Scripts/Movement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Rigidbody2D mario;
+    public static bool guapo = false;
+    SpriteRenderer marioGrande;
+    string nameOfSprite;
+    public Sprite sprite2;
+    public AudioSource forward;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mario = GetComponent<Rigidbody2D>();
+        marioGrande = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            jump();
+    }
+
+    void jump()
+    {
+        if (mario)
+        {
+            if (Mathf.Abs(mario.velocity.y) < 0.05f)
+                mario.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(mario)
+        {
+            mario.AddForce(new Vector2(Input.GetAxis("Horizontal") * 10, 0));
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            if (transform.rotation != Quaternion.Euler(0, 180, 0))
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            if (transform.rotation != Quaternion.Euler(0, 0, 0))
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+            Destroy(gameObject);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Mushroom"))
+        {
+            guapo = !guapo;
+            forward.Play();
+            marioGrande.sprite = sprite2;
+            Destroy(other.gameObject);
+        }
+    }
+
+
+}
+
